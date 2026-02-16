bcb7f8540fa514ced52f7810c088ea31ed10471d
sainukala571 <sainukala5@gmail.com>
2026-02-16 12:32:55 +0530

    class

--- admin/Class/edit_class.php
+++ admin/Class/edit_class.php
@@ -1,26 +1,39 @@
 <?php require_once(__DIR__ . '/../../config.php'); ?>
+
 <?php 
 
+// require_once("libraries/functions.class.php");
 require_once(LIB_PATH . '/functions.class.php');
 
 $fcObj = new DataFunctions();
 
 $tbClass = TB_CLASS;
 
-$classes    = $fcObj->getClasses($tbClass);
+/* ---------------- Fetch All Classes ---------------- */
+$classes = $fcObj->getClasses($tbClass);
 $classesCnt = sizeof($classes);
 
-$classDet = array(); // Prevent undefined variable error
+
+/* ---------------- Get Class Details ---------------- */
+$classDet = [];
 
 if (isset($_GET['class']) && !empty($_GET['class'])) {
 
-    $clsId    = intval($_GET['class']);
+    $clsId = intval($_GET['class']); // Security
+
     $classDet = $fcObj->getClassById($tbClass, $clsId);
+
+    if (empty($classDet)) {
+        die("Invalid Class ID");
+    }
 }
 
+
+/* ---------------- Update Class ---------------- */
 if (isset($_POST['editClass'])) {
 
-    $varArray = array();
+    $varArray = [];
+
     $varArray['class_id']   = intval($_POST['classId']);
     $varArray['class_name'] = trim($_POST['className']);
     $varArray['class_code'] = trim($_POST['classCode']);
@@ -30,7 +43,7 @@ if (isset($_POST['editClass'])) {
     if ($editClass) {
 
         header('Location: otheroperations.php');
-        exit; // stop execution properly
+        exit;
 
     } else {
 
@@ -39,22 +52,28 @@ if (isset($_POST['editClass'])) {
     }
 }
 
+
 include_once('../layout/main_header.php');
+
 ?>
 
 <div id="page">
     <div id="content">
+
         <div class="post">
             <span class="alignCenter">
-                <h4>AIML Department </h4>
+                <h4>MBA Department</h4>
             </span>
+            <p></p>
         </div>
 
         <div id='content_left' class='content_left'>
             <?php include_once('../layout/other_leftnav.php'); ?>
         </div>
 
+
         <div id='content_right' class='content_right'>
+
             <div class="comteeMem">
 
                 <?php if (isset($msg)) { ?>
@@ -65,54 +84,59 @@ include_once('../layout/main_header.php');
                     </div>
                 <?php } ?>
 
-                <?php if (!empty($classDet)) { ?>
 
                 <form id='editclass' action='edit_class.php' method='POST' accept-charset='UTF-8' enctype="multipart/form-data">
 
                     <div class="form_row">
                         <div class="form_label">
-                            <label>Class Code :</label>
+                            <label for="classcode">Class Code :</label>
                         </div>
                         <div class="form_field">
                             <input type="text" name="classCode" id="classCode"
-                                value="<?php echo htmlspecialchars($classDet[0]['class_code']); ?>" />
+                                value="<?php echo isset($classDet[0]['class_code']) ? $classDet[0]['class_code'] : ''; ?>" />
                         </div>
                     </div>
+
 
                     <div class="form_row">
                         <div class="form_label">
-                            <label>Class Name :</label>
+                            <label for="classname">Class Name :</label>
                         </div>
                         <div class="form_field">
                             <input type="text" name="className" id="className"
-                                value="<?php echo htmlspecialchars($classDet[0]['class_name']); ?>" />
+                                value="<?php echo isset($classDet[0]['class_name']) ? $classDet[0]['class_name'] : ''; ?>" />
                         </div>
                     </div>
 
+
                     <div class="form_row">
                         <div class="form_label"></div>
+
                         <div class="form_field">
+
                             <input type="hidden" name="classId" id="classId"
-                                value="<?php echo intval($classDet[0]['id']); ?>" />
+                                value="<?php echo isset($classDet[0]['id']) ? $classDet[0]['id'] : ''; ?>" />
+
                             <input type='submit' name='editClass' class="button" value='Update Class' />
+
                         </div>
                     </div>
 
                 </form>
 
-                <?php } else { ?>
-                    <div class="usersDetHeader">Invalid Class Selected</div>
-                <?php } ?>
-
             </div>
         </div>
 
+
         <br class="clearfix" />
+
     </div>
 
     <?php include_once('../layout/sidebar.php'); ?>
 
     <br class="clearfix" />
+</div>
+
 </div>
 
 <?php include_once('../layout/footer.php'); ?>
